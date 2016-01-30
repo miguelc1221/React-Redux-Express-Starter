4167f8f4d6032f80d1b11e7663c24b43c97d9a4f
Miguel Correa <miguelc1221@gmail.com>
2016-01-29 21:53:09 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+<h1>My React starter package</h1>
---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+
+import App from './component/app.js';
+import reducers from './reducers';
+
+const store = createStore(
+    reducers,
+    applyMiddleware()
+);
+
+render(
+    <Provider store={store}>
+        <App />,
+    </Provider>,
+    document.getElementById('root')
+);
