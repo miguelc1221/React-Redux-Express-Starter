28d8b2b9111655fd74f0b7764e2f86df5b0c0638
Miguel Correa <miguelc1221@gmail.com>
2016-02-06 02:07:24 -0500

    update package.json

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import { Route, browserHistory } from 'react-router';
+import { Router, browserHistory } from 'react-router';
 
 import routes from './routes.js';
 import reducers from './reducers';
@@ -13,7 +13,7 @@ const store = createStore(
 
 render(
     <Provider store={store}>
-        <Route history={browserHistory} routes={routes}/>
+        <Router history={browserHistory} routes={routes}/>
     </Provider>,
     document.getElementById('root')
 );
