6a81d22fbcc28327c7edb26bb549c0080656aee0
Miguel Correa <miguelc1221@gmail.com>
2016-04-27 11:01:49 -0400

    replace karma with jsdom

--- test/app.test.js
+++ test/app.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import App from '../src/component/app';
+import { expect } from 'chai';
+import App from '../app/component/app';
 
 describe('App', () => {
     const wrapper = shallow(<App />)
